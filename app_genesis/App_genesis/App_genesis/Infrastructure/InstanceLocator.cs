fa0f87d70c77b9b9da7766dfd15c73ef6d0ec10a
Jhon Romero <edisson5jhon@gmail.com>
2018-07-19 23:01:34 -0500

    Login page

---
+++ app_genesis/App_genesis/App_genesis/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using App_genesis.ViewModels;
+
+namespace App_genesis.Infrastructure
+{
+    class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main { get; set; }
+        #endregion
+
+        #region Constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
