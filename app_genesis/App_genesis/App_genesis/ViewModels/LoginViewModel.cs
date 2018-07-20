c07b10bc7b739646123f2c83bc25084392a141ab
Jhon Romero <edisson5jhon@gmail.com>
2018-07-20 17:52:13 -0500

    Login view model with base view model

--- app_genesis/App_genesis/App_genesis/ViewModels/LoginViewModel.cs
+++ app_genesis/App_genesis/App_genesis/ViewModels/LoginViewModel.cs
@@ -1,31 +1,47 @@
 ﻿namespace App_genesis.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.Text;
     using System.Windows.Input;
-    class LoginViewModel
+    using Xamarin.Forms;
+
+    public class LoginViewModel : BaseViewModel
     {
+
+        #region Attributes
+        public string password;
+        public bool isRunning;
+        public bool isEnabled;
+        #endregion
+
         #region Properties
-        public int Id
+        public string Id
         {
             get;
             set;
         }
         public string Password
         {
-            get;
-            set;
+            get { return password; }
+            set { SetValue(ref password, value); }
+
         }
         public bool IsRunning
         {
-            get;
-            set;
+            get { return isRunning; }
+            set { SetValue(ref isRunning, value); }
         }
         public bool IsRemembered
         {
             get;
             set;
+        }
+        public bool IsEnabled {
+            get { return isEnabled; }
+            set { SetValue(ref isEnabled, value); }
         }
         #endregion
 
@@ -33,14 +49,61 @@
         public LoginViewModel()
         {
             this.IsRemembered = true;
+            this.isEnabled = true;
         }
         #endregion
 
         #region Commands
         public ICommand LoginCommand
         {
-            get;
-            set;
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Id))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Debe ingresar el Id.",
+                    "Aceptar");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Debe ingresar la Contraseña.",
+                    "Aceptar");
+                return;
+            }
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            if (this.Id != "000261755" || this.Password != "1234")
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Id o Password incorrectos",
+                    "Aceptar");
+                this.Password = string.Empty;
+                return;
+            }
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+
+            await Application.Current.MainPage.DisplayAlert(
+                    "Ok",
+                    "Bienvenido",
+                    "Aceptar");
         }
         #endregion
 
